8d68eac473e4e1adc7f910038c58eb9339018598
Alcyeron <f20230588@pilani.bits-pilani.ac.in>
2024-02-20 17:23:17 +0530

    add aditional binning options

--- q1.py
+++ q1.py
@@ -5,20 +5,22 @@ img = Image.open('clock_tower.png')
 
 img_arr = np.array(img)
 
-img_arr = img_arr.astype('uint8')
+img_arr = img_arr.astype('uint64')
 
-print(img_arr.shape)
+print(img_arr)
 
-# for i in range(img_arr.shape[0]-1):
-#     for j in range(img_arr.shape[1]-1):
-#         for k in range(3):
-#             img_arr[i][j][k] = (img_arr[i][j][k] + img_arr[i][j + 1][k] + img_arr[i + 1][j][k] + img_arr[i + 1][j + 1][k]) // 4
+print("----------------------------------")
+
 # n = int(input("Enter the scale factor for binning: "))
 # new_image = img_arr[::n, ::n]
-# new_image = (img_arr[::2, ::2] + img_arr[1::2, ::2] + img_arr[::2, 1::2] + img_arr[1::2, 1::2]) // 4
-new_image = np.maximum(np.maximum(img_arr[::2, ::2], img_arr[1::2, ::2]),
-                       np.maximum(img_arr[::2, 1::2], img_arr[1::2, 1::2]))
-# new_image = np.minimum(np.minimum(img_arr[::2, ::2], img_arr[1::2, ::2]), np.minimum(img_arr[::2, 1::2], img_arr[1::2, 1::2]))
+new_image = (img_arr[::2, ::2] + img_arr[1::2, ::2] + img_arr[::2, 1::2] + img_arr[1::2, 1::2]) // 4
+# new_image = (img_arr[::2, ::2] + img_arr[::2, 1::2]) // 2
+# new_image = np.maximum(np.maximum(img_arr[::2, ::2], img_arr[1::2, ::2]),
+#                        np.maximum(img_arr[::2, 1::2], img_arr[1::2, 1::2]))
+# new_image = np.minimum(np.minimum(img_arr[::2, ::2], img_arr[1::2, ::2]), np.minimum(img_arr[::2, 1::2],
+#                                                                                      img_arr[1::2, 1::2]))
+
+print(new_image)
 out_img = Image.fromarray(new_image.astype('uint8'))
 
 out_img.save('output_image.png')
